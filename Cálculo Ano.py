f7dd44b0238be7f83cd39fa5626c71de355749e1
Vinícius Campos <vinicxcampos@gmail.com>
2023-08-11 22:08:35 -0300

    Primeira versão do programa, já executável

---
+++ Cálculo Ano.py
@@ -0,0 +1,14 @@
+#Código
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+saldo = float(input("Digite seu saldo: "))
+perc = int (input("Digite o percentual de aumento: "))
+ano = 2023 - idade
+saldopc = (saldo*perc)/100 + saldo
+
+
+#Print
+print (nome, "o seu saldo é: ", saldo)
+print (nome, "o seu saldo com acréscimo de", perc, "é: ", saldopc)
+print (nome, "o ano que você nasceu é: ",2023 - idade) 
+print (f"Você {nome} que nasceu em {ano} terá um saldo de {saldopc}R$ ,devido ao aumento de {perc}%")
